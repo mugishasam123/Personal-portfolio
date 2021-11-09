3e9d582b79a8caaf8c3b881df40ea9781e56baa7
mugisha Samuel <mugishasamuel42@gmail.com>
2021-11-09 11:54:11 -0800

    linters check

--- menu.js
+++ menu.js
@@ -1,41 +1,27 @@
 const hamb = document.querySelector('.hamburger');
-
 const closebt = document.querySelector('.close');
-
 const first = document.querySelector('#intro');
-
 const menu = document.querySelector('.nav-menu');
-
 const link1 = document.querySelector('.nav1');
-
 const link2 = document.querySelector('.nav2');
-
 const link3 = document.querySelector('.nav3');
-
 hamb.addEventListener('click', () => {
- first.style.display = 'none';
- menu.style.display = 'flex';
-
+  first.style.display = 'none';
+  menu.style.display = 'flex';
 });
-
 closebt.addEventListener('click', () => {
-   first.style.display = 'flex';
-   menu.style.display = 'none';
-  
-  });
-
-  link1.addEventListener('click', () => {
-   first.style.display = 'flex';
-   menu.style.display = 'none';
-  });
-
-  link2.addEventListener('click', () => {
-   first.style.display = 'flex';
-   menu.style.display = 'none';
-
-  });
-
-  link3.addEventListener('click', () => {
-   first.style.display = 'flex';
-   menu.style.display = 'none';
-  });
+  first.style.display = 'flex';
+  menu.style.display = 'none';
+});
+link1.addEventListener('click', () => {
+  first.style.display = 'flex';
+  menu.style.display = 'none';
+});
+link2.addEventListener('click', () => {
+  first.style.display = 'flex';
+  menu.style.display = 'none';
+});
+link3.addEventListener('click', () => {
+  first.style.display = 'flex';
+  menu.style.display = 'none';
+});
